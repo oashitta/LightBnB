685ca60f7302505fb40ff54b20aa79698a5b3d64
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-05-04 15:18:41 -0400

    get all my reservations

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -112,7 +112,29 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `SELECT reservations.*, properties.*, AVG(property_reviews.rating)
+  FROM reservations
+  JOIN properties 
+  ON reservations.property_id = properties.id
+  JOIN property_reviews 
+  ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY reservations.id, properties.title, reservations.start_date, properties.cost_per_night, properties.id
+  ORDER BY start_date ASC
+  LIMIT 10;`
+
+  return pool
+    // .query(queryString, values)
+    .query(queryString, [limit])
+    .then((result) => {
+      // console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+  // return getAllProperties(null, 2);
 };
 
 /// Properties
